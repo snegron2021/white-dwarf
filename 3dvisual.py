1f43dc5814a0b8993239f7382ffa9f9f83fd6679
Shailyn Negron <shailynnegron@Shailyns-iMac-3.local>
2024-12-07 00:28:52 -0500

    Rename app.py to 3dvisual.py and remove app.py from tracking

--- 3dvisual.py
+++ 3dvisual.py
@@ -4,28 +4,22 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 
-# Read the data
 data = pd.read_csv("/Users/shailynnegron/Desktop/Alldata.csv")
 
-# Calculate distance in parsecs
 data['distance'] = 1 / data['parallax']
 
-# Cartesian coordinate calculations (parsecs)
 data['x'] = (data['distance']) * np.cos(np.radians(data['b'])) * np.cos(np.radians(data['l']))  # x
 data['y'] = (data['distance']) * np.cos(np.radians(data['b'])) * np.sin(np.radians(data['l']))  # y
 data['z'] = (data['distance']) * np.sin(np.radians(data['b']))  # z
 
-# Define temperature bins
 bins = [5500, 6000, 7000, 10000, 15000, 40000]
 labels = ['Cool', 'Moderate', 'Hot', 'Very Hot', 'Extremely Hot']
 data['temperature_bin'] = pd.cut(data['teff_gspphot'], bins=bins, labels=labels, right=False)
 
-# Create a new DataFrame with temperature and coordinates
 df = data[['x', 'y', 'z', 'temperature_bin', 'teff_gspphot']].copy()
 df['bin_numeric'] = df['temperature_bin'].cat.codes
 df_sample = df.sample(frac=0.01)
 
-# Create the Plotly 3D scatter plot
 fig = px.scatter_3d(
     df_sample,
     x='x',
@@ -39,15 +33,12 @@ fig = px.scatter_3d(
 
 fig.update_traces(marker=dict(size=2))
 
-# Initialize the Dash app
 app = dash.Dash(__name__)
 
-# Define the layout of the app
 app.layout = html.Div([
     html.H1("Interactive 3D White Dwarf Distribution"),
     dcc.Graph(figure=fig)
 ])
 
-# Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
